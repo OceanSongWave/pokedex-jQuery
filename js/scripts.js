19e59aecab25cc198386f398d5ec04cbe9589b8a
OceanSongWave <65198135+OceanSongWave@users.noreply.github.com>
2020-10-23 23:38:23 -0400

    Added content in scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello World!');
-
-let favoriteFood = 'tonkastu miso ramen';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Charmander",
+    height: 2.00,
+    types: ['fire', 'blaze']
+  },
+  {
+    name: "Sandshrew",
+    height: 2.00,
+    types: ['ground', 'sand veil']
+  },
+  {
+    name: "Wigglytuff",
+    height: 3.03,
+    types: ['fairy', 'cute charm', 'competitive']
+  },
+  {
+    name: "Bayleef",
+    height: 3.11,
+    types: ['grass', 'overgrow']
+  },
+  {
+    name: "Altaria",
+    height: 3.07,
+    types: ['dragon', 'flying', 'natural cure']
+  }
+]
