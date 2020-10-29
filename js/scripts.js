8e78a263b4306661d5fb4e852da95a56238dd24e
OceanSongWave <65198135+OceanSongWave@users.noreply.github.com>
2020-10-29 00:06:18 -0400

    Added loop & content highlights

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,72 @@
 let pokemonList = [
   {
     name: "Charmander",
-    height: 2.00,
-    types: ['fire', 'blaze']
+    height: 4.0,
+    types: ["fire", "blaze"],
   },
   {
     name: "Sandshrew",
-    height: 2.00,
-    types: ['ground', 'sand veil']
+    height: 1.7,
+    types: ["ground", "sand veil"],
   },
   {
     name: "Wigglytuff",
-    height: 3.03,
-    types: ['fairy', 'cute charm', 'competitive']
+    height: 1.5,
+    types: ["fairy", "cute charm", "competitive"],
   },
   {
     name: "Bayleef",
-    height: 3.11,
-    types: ['grass', 'overgrow']
+    height: 3.5,
+    types: ["grass", "overgrow"],
   },
   {
     name: "Altaria",
-    height: 3.07,
-    types: ['dragon', 'flying', 'natural cure']
+    height: 7.0,
+    types: ["dragon", "flying", "natural cure"],
+  },
+];
+
+// set up for loop on pokemonList
+// add color to different types
+for (let i = 0; i < pokemonList.length; i++) {
+  let color = "";
+  for (let k = 0; k < pokemonList[i].types.length; k++) {
+    if (pokemonList[i].types[k] === "grass") {
+      color = '<span style="color:green;"> ';
+    } else if (pokemonList[i].types[k] === "fire") {
+      color = '<span style="color:red;">';
+    } else if (pokemonList[i].types[k] === "ground") {
+      color = '<span style="color:brown;">';
+    } else if (pokemonList[i].types[k] === "fairy") {
+      color = '<span style="color:lightblue;">';
+    } else if (pokemonList[i].types[k] === "dragon") {
+      color = '<span style="color:orange;">';
+    }
   }
-]
+
+  let size = "";
+  if (pokemonList[i].height > 6) {
+    size = "Wow, what a big pokemon!";
+  } else if (pokemonList[i].height < 2) {
+    size = "Aww, a little pokemon!";
+  } else {
+    size = "An average size pokemon";
+  }
+
+
+ // set up box for each pokemon & its content 
+  document.write(
+    '<div class = "box">' +
+      pokemonList[i].name +
+      " (height: " +
+      pokemonList[i].height +
+      ")" +
+      size +
+      color +
+      "<br>" +
+      pokemonList[i].types +
+      "<br>" +
+      "</div>"
+  );
+}
+
