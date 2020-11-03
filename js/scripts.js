2c75b853fecacec46fa1ffaa9f37950ed0b12d17
OceanSongWave <65198135+OceanSongWave@users.noreply.github.com>
2020-11-03 16:47:40 -0500

    Updated IIFE function

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,93 @@
-let pokemonList = [
-  {
-    name: "Charmander",
-    height: 4.0,
-    types: ["fire", "blaze"],
-  },
-  {
-    name: "Sandshrew",
-    height: 1.7,
-    types: ["ground", "sand veil"],
-  },
-  {
-    name: "Wigglytuff",
-    height: 1.5,
-    types: ["fairy", "cute charm", "competitive"],
-  },
-  {
-    name: "Bayleef",
-    height: 3.5,
-    types: ["grass", "overgrow"],
-  },
-  {
-    name: "Altaria",
-    height: 7.0,
-    types: ["dragon", "flying", "natural cure"],
-  },
-];
+let pokemonRepository = (function(){
+  let pokemonList = [
+    {
+      name: "Charmander",
+      height: 4.0,
+      types: ["fire", "blaze"],
+    },
+    {
+      name: "Sandshrew",
+      height: 1.7,
+      types: ["ground", "sand veil"],
+    },
+    {
+      name: "Wigglytuff",
+      height: 1.5,
+      types: ["fairy", "cute charm", "competitive"],
+    },
+    {
+      name: "Bayleef",
+      height: 3.5,
+      types: ["grass", "overgrow"],
+    },
+    {
+      name: "Altaria",
+      height: 7.0,
+      types: ["dragon", "flying", "natural cure"],
+    },
+  ];
+  function getAll (){
+    return pokemonList
+  }
+  function add (pokemon){
+    if(
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ){
+      pokemonList.push(pokemon)
+    }else {
+      alert("Please enter valid pokemon")
+    }
+    
+  }
+  return {
+    getAll:getAll,
+    add:add,
+  }
+})()
 
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name:"Eve", height: 4.5, types: ["speed"]})
+console.log(pokemonRepository.getAll());
 // set up forEach loop on pokemonList
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
+  
+    let color = "";
+      pokemon.types.forEach(function(type){
+        if (type === "grass") {
+          color = '<span style="color:green;"> ';
+        } else if (type === "fire") {
+          color = '<span style="color:red;">';
+        } else if (type === "ground") {
+          color = '<span style="color:brown;">';
+        } else if (type === "fairy") {
+          color = '<span style="color:lightblue;">';
+        } else if (type === "dragon") {
+          color = '<span style="color:orange;">';
+        } else if (type === "speed") {
+          color = '<span style="color:blue;">';
+        }
+      })
+      
+
+    let size = "";
+    if (pokemon.height > 6) {
+      size = "Wow, what a big pokemon!";
+    } else if (pokemon.height < 2) {
+      size = "Aww, a little pokemon!";
+    } else {
+      size = "An average size pokemon";
+    }
+
   document.write(
     '<div class = "box">' +
     pokemon.name + 
     " has height of " +
     pokemon.height +
+    size +
+    color +
     "<br>" + 
     "Types: " +
     pokemon.types + 
@@ -42,44 +97,6 @@ pokemonList.forEach(function(pokemon) {
 });
 
 // add color to different types
-// for (let i = 0; i < pokemonList.length; i++) {
-//   let color = "";
-//   for (let k = 0; k < pokemonList[i].types.length; k++) {
-//     if (pokemonList[i].types[k] === "grass") {
-//       color = '<span style="color:green;"> ';
-//     } else if (pokemonList[i].types[k] === "fire") {
-//       color = '<span style="color:red;">';
-//     } else if (pokemonList[i].types[k] === "ground") {
-//       color = '<span style="color:brown;">';
-//     } else if (pokemonList[i].types[k] === "fairy") {
-//       color = '<span style="color:lightblue;">';
-//     } else if (pokemonList[i].types[k] === "dragon") {
-//       color = '<span style="color:orange;">';
-//     }
-//   }
-
-//   let size = "";
-//   if (pokemonList[i].height > 6) {
-//     size = "Wow, what a big pokemon!";
-//   } else if (pokemonList[i].height < 2) {
-//     size = "Aww, a little pokemon!";
-//   } else {
-//     size = "An average size pokemon";
-//   }
 
 
-//   // set up box for each pokemon & its content 
-//   document.write(
-//     '<div class = "box">' +
-//       pokemonList[i].name +
-//       " (height: " +
-//       pokemonList[i].height +
-//       ")" +
-//       size +
-//       color +
-//       "<br>" +
-//       pokemonList[i].types +
-//       "<br>" +
-//       "</div>"
-//   );
-// }
+
