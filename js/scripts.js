d8c801c921153d16787008357a5d03c4a096cf52
OceanSongWave <65198135+OceanSongWave@users.noreply.github.com>
2020-11-03 19:18:01 -0500

    Updated js file

--- js/scripts.js
+++ js/scripts.js
@@ -26,10 +26,11 @@ let pokemonRepository = (function(){
       types: ["dragon", "flying", "natural cure"],
     },
   ];
-  function getAll (){
-    return pokemonList
+  
+  function getAll() {
+    return pokemonList;
   }
-  function add (pokemon){
+  function add(pokemon) {
     if(
       typeof pokemon === "object" &&
       "name" in pokemon &&
@@ -38,65 +39,62 @@ let pokemonRepository = (function(){
     ){
       pokemonList.push(pokemon)
     }else {
-      alert("Please enter valid pokemon")
+      alert("Please enter valid Pokemon")
     }
-    
   }
   return {
-    getAll:getAll,
-    add:add,
+    getAll: getAll,
+    add: add,
   }
-})()
+})();
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({name:"Eve", height: 4.5, types: ["speed"]})
-console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: "Jolteon", height: 2.5, types: ["lightning", "volt absor"]})
 // set up forEach loop on pokemonList
-pokemonRepository.getAll().forEach(function(pokemon) {
-  
-    let color = "";
-      pokemon.types.forEach(function(type){
-        if (type === "grass") {
-          color = '<span style="color:green;"> ';
-        } else if (type === "fire") {
-          color = '<span style="color:red;">';
-        } else if (type === "ground") {
-          color = '<span style="color:brown;">';
-        } else if (type === "fairy") {
-          color = '<span style="color:lightblue;">';
-        } else if (type === "dragon") {
-          color = '<span style="color:orange;">';
-        } else if (type === "speed") {
-          color = '<span style="color:blue;">';
-        }
-      })
-      
+console.log(pokemonRepository.getAll());
 
+pokemonRepository.getAll().forEach(function(pokemon) {
+// add color to different types
+  let color = "";
+    pokemon.types.forEach(function(type){
+      if (type === "grass") {
+        color = '<span style="color:green;"> ';
+      } else if (type === "fire") {
+        color = '<span style="color:red;">';
+      } else if (type === "ground") {
+        color = '<span style="color:brown;">';
+      } else if (type === "fairy") {
+        color = '<span style="color:lightblue;">';
+      } else if (type === "dragon") {
+        color = '<span style="color:orange;">';
+      } else if (type === "lightning") {
+        color = '<span style="color:blue;">';
+      }
+    })
+      
     let size = "";
-    if (pokemon.height > 6) {
-      size = "Wow, what a big pokemon!";
-    } else if (pokemon.height < 2) {
-      size = "Aww, a little pokemon!";
-    } else {
-      size = "An average size pokemon";
-    }
+      if (pokemon.height > 6) {
+        size = "Wow, what a big pokemon!";
+      } else if (pokemon.height < 2) {
+        size = "Aww, a little pokemon!";
+      } else {
+        size = "An average size pokemon";
+      }
 
   document.write(
     '<div class = "box">' +
     pokemon.name + 
     " has height of " +
     pokemon.height +
+    "<br>" +
     size +
     color +
     "<br>" + 
     "Types: " +
-    pokemon.types + 
-    "." +
+    pokemon.types +
     '</div>'
   );
 });
-
-// add color to different types
 
 
 
