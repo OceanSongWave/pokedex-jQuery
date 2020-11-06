265768d07e3a73c456bf4a8862762e1cf63c4ed3
OceanSongWave <65198135+OceanSongWave@users.noreply.github.com>
2020-11-05 22:51:15 -0500

    Update UI, add event listener

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,7 @@ let pokemonRepository = (function(){
   function getAll() {
     return pokemonList;
   }
+
   function add(pokemon) {
     if(
       typeof pokemon === "object" &&
@@ -42,59 +43,37 @@ let pokemonRepository = (function(){
       alert("Please enter valid Pokemon")
     }
   }
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
   return {
     getAll: getAll,
     add: add,
+    addListItem: addListItem
   }
 })();
 
 console.log(pokemonRepository.getAll());
 pokemonRepository.add({name: "Jolteon", height: 2.5, types: ["lightning", "volt absor"]})
-// set up forEach loop on pokemonList
+
 console.log(pokemonRepository.getAll());
 
+// set up forEach loop on pokemonList
 pokemonRepository.getAll().forEach(function(pokemon) {
-// add color to different types
-  let color = "";
-    pokemon.types.forEach(function(type){
-      if (type === "grass") {
-        color = '<span style="color:green;"> ';
-      } else if (type === "fire") {
-        color = '<span style="color:red;">';
-      } else if (type === "ground") {
-        color = '<span style="color:brown;">';
-      } else if (type === "fairy") {
-        color = '<span style="color:lightblue;">';
-      } else if (type === "dragon") {
-        color = '<span style="color:orange;">';
-      } else if (type === "lightning") {
-        color = '<span style="color:blue;">';
-      }
-    })
-      
-    let size = "";
-      if (pokemon.height > 6) {
-        size = "Wow, what a big pokemon!";
-      } else if (pokemon.height < 2) {
-        size = "Aww, a little pokemon!";
-      } else {
-        size = "An average size pokemon";
-      }
-
-  document.write(
-    '<div class = "box">' +
-    pokemon.name + 
-    " has height of " +
-    pokemon.height +
-    "<br>" +
-    size +
-    color +
-    "<br>" + 
-    "Types: " +
-    pokemon.types +
-    '</div>'
-  );
+  pokemonRepository.addListItem(pokemon);
 });
-
-
-
