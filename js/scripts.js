78f67d8427de3fbdbfca2c04f0017110fe81a97c
OceanSongWave <65198135+OceanSongWave@users.noreply.github.com>
2020-11-09 16:52:04 -0500

    Ajax

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,23 @@
-let pokemonRepository = (function(){
-  let pokemonList = [
-    {
-      name: "Charmander",
-      height: 4.0,
-      types: ["fire", "blaze"],
-    },
-    {
-      name: "Sandshrew",
-      height: 1.7,
-      types: ["ground", "sand veil"],
-    },
-    {
-      name: "Wigglytuff",
-      height: 1.5,
-      types: ["fairy", "cute charm", "competitive"],
-    },
-    {
-      name: "Bayleef",
-      height: 3.5,
-      types: ["grass", "overgrow"],
-    },
-    {
-      name: "Altaria",
-      height: 7.0,
-      types: ["dragon", "flying", "natural cure"],
-    },
-  ];
-  
+let pokemonRepository = (function () {
+  let pokemonList = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
+
   function getAll() {
     return pokemonList;
   }
 
   function add(pokemon) {
-    if(
-      typeof pokemon === "object" &&
-      "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
-    ){
-      pokemonList.push(pokemon)
-    }else {
-      alert("Please enter valid Pokemon")
+    if (typeof pokemon === "object") {
+      pokemonList.push(pokemon);
+    } else {
+      alert("Please enter valid Pokemon");
     }
   }
 
   function addListItem(pokemon) {
     let pokemonList = document.querySelector(".pokemon-list");
     let listItem = document.createElement("li");
+    listItem.classList.add("list");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button-class");
@@ -58,22 +29,81 @@ let pokemonRepository = (function(){
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function(){
+      console.log(pokemon);
+    })
   }
+
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = [];
+      details.types.forEach(function(itemType){
+        item.types.push(itemType.type.name);
+      })
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
 
   return {
     getAll: getAll,
     add: add,
-    addListItem: addListItem
-  }
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+  };
 })();
 
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({name: "Jolteon", height: 2.5, types: ["lightning", "volt absor"]})
-
-console.log(pokemonRepository.getAll());
-
-// set up forEach loop on pokemonList
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  // set up forEach loop on pokemonList
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
+
+function search() {
+  let input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.querySelectorAll(".list");
+  // console.log(li);
+  // console.log(li[0].querySelector("#test").getElementsByTagName("button")[0]);
+  // li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("button")[0];
+    console.log(a.innerText);
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
